57945f7ea653172e3638159d04b3ce7abaf7dc2d
Nicolas <nicolas.tual@outlook.fr>
2024-06-25 13:11:17 +0200

    Add GoodieComponent, DashboardComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoodiesComponent } from './goodies/goodies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: GoodiesComponent,
+  title: 'Personnalisez votre goodie'
+}, {
+  path: 'dashboard',
+  component: DashboardComponent,
+  title: 'Tableau de bord'
+}];
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7d9695',
+        secondary: '#40615e',
+        ternary: '#2d4d4a'
+      }
+    },
+  },
+  plugins: [],
+}
+
